5a650460f58e554f25efa25451958e64ea7a1eca
seo02wow <rlatjdud9188@naver.com>
2024-07-13 14:24:26 +0900

    :art:style: black han sans, gowunBatang 폰트 추가

---
+++ app/font.ts
@@ -0,0 +1,11 @@
+import { Black_Han_Sans, Gowun_Batang } from "next/font/google";
+
+export const gowunBatang = Gowun_Batang({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const blackHanSans = Black_Han_Sans({
+  subsets: ["latin"],
+  weight: ["400"],
+});
